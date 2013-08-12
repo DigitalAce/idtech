57c36716030a709e351002fafe3ba0b30cb74b7b
Donovan Roudabush <sharksfan98@gmail.com>
2013-08-12 10:42:27 -0700

    Create hello_world.cpp

---
+++ Day-1/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello world!\n";
+	cout << "What is your name?\n";
+	string name;
+	cin >> name;
+	cout << "Your name is"
+		<< name << "!\n";
+}
