5dfcbdf32eb80472bbef0778ae8ad3afa9205775
Donovan Roudabush <sharksfan98@gmail.com>
2013-08-13 11:01:15 -0700

    Create game.cpp

---
+++ Day-2/game.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	// Variables
+	char board [10] = {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
+	int playerx = 2;
+	char playericon = 1;
+
+	while (true)
+	{
+		char input = getch();
+			if (input == 'a')
+			{
+				playerx--;
+			}
+			else if (input == 'd')
+			{
+				playerx++;
+			}
+			system("cls");
+			int i = 0;
+			while(i<0)
+			{
+				if(i == playerx)
+				{
+					cout << playericon;
+				}
+				else
+				{
+					cout << board [;];
+				}
+				i++;
+			}
+			cout << "\n";
+			// Frame rate limiter
+			Sleep(50);
+	}
+}
